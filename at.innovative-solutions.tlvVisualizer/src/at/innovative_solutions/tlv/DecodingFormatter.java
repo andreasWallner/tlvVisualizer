2bdf5f5321e049ab300fe62fae3a4056ac52d66b
Andreas Wallner <A.Wallner@innovative-solutions.at>
2016-11-08 00:48:53 +0100

    Cleanup DecodingFormatter

--- at.innovative-solutions.tlvVisualizer/src/at/innovative_solutions/tlv/DecodingFormatter.java
+++ at.innovative-solutions.tlvVisualizer/src/at/innovative_solutions/tlv/DecodingFormatter.java
@@ -1,19 +1,15 @@
 package at.innovative_solutions.tlv;
 
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
 import java.util.List;
 
-import at.innovative_solutions.tlvvisualizer.views.TagInfo;
-
 public class DecodingFormatter implements Formatter<String> {
-	final String _indentChars;
-	int _indentLevel;
+	final String fIndentChars;
+	int fIndentLevel;
 	final ValueDecoder fDecoder;
 	
 	public DecodingFormatter(final String indentChars, ValueDecoder decoder) {
-		_indentChars = indentChars;
-		_indentLevel = 0;
+		fIndentChars = indentChars;
+		fIndentLevel = 0;
 		fDecoder = decoder;
 	}
 	
@@ -34,17 +30,16 @@ public class DecodingFormatter implements Formatter<String> {
 		final StringBuilder sb = new StringBuilder();
 		
 		String idString = Utils.bytesToHexString(tlv.getID().toBytes());
-		Long id = tlv.getID().toLong();
 		String encoded = Utils.bytesToHexString(tlv.getData());
 		
-		sb.append(Utils.printChars(_indentChars, _indentLevel));
+		sb.append(Utils.printChars(fIndentChars, fIndentLevel));
 		sb.append(idString);
 		sb.append(" > ");
 		String name = fDecoder.getName(tlv);
 		if(name != null) {
 			sb.append(name);
 			sb.append("\n");
-			sb.append(Utils.printChars(_indentChars, _indentLevel));
+			sb.append(Utils.printChars(fIndentChars, fIndentLevel));
 			sb.append(Utils.printChars(" ", idString.length() + 3));
 			
 			String decoded = fDecoder.toString(tlv);
@@ -65,19 +60,17 @@ public class DecodingFormatter implements Formatter<String> {
 	public String format(final ConstructedTLV tlv) {
 		final StringBuilder sb = new StringBuilder();
 		
-		Long id = tlv.getID().toLong();
-		
-		sb.append(Utils.printChars(_indentChars, _indentLevel));
+		sb.append(Utils.printChars(fIndentChars, fIndentLevel));
 		sb.append(Utils.bytesToHexString(tlv.getID().toBytes()));
 		
 		sb.append(" : ").append(fDecoder.getName(tlv));
 		
 		sb.append("\n");
-		_indentLevel += 1;
+		fIndentLevel += 1;
 		
 		for(TLV child : tlv.getTLVs())
 			sb.append(child.accept(this));
-		_indentLevel -= 1;
+		fIndentLevel -= 1;
 		return sb.toString();
 	}
 	
@@ -85,7 +78,7 @@ public class DecodingFormatter implements Formatter<String> {
 	@Override
 	public String format(final ErrorTLV tlv) {
 		final StringBuilder sb = new StringBuilder();
-		sb.append(Utils.printChars(_indentChars, _indentLevel));
+		sb.append(Utils.printChars(fIndentChars, fIndentLevel));
 		sb.append("ERROR");
 		return sb.toString();
 	}
